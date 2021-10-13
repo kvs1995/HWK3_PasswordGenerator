37e55a3679cf6eca07627f0bdce5afa0233ed11d
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-10-12 19:01:50 -0500

    testing out the var objects as a way to store the criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Generate Password Function
+  //
+
+var test = {
+  length: 8,
+  lowercase: false,
+  uppercase: false,
+  numeric: false,
+  specialCharacters: false   
+};
+
+console.log(test.length)
+
+test.length = 9;
+console.log(test.length)
