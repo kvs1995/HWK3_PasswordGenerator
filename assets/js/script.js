dfef0166f3448ed76086e4fb956c78249a80fd73
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-10-16 15:49:35 -0500

    removed unnecessary code that was commented out, added information comments to provide reasoning.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-//Generate Password Function Parts
-  //(1) STORE criteria in an object all together
-
 var criteria = {
   length: 8,
   lowercase: false,
@@ -26,15 +22,8 @@ var criteria = {
   specialCharacters: false   
 };
 
+//select a random character functions for each type of character
 
-// console.log(criteria.length);
-//confirm you can change the object value
-  // criteria.length = 9;
-  // console.log(criteria.length) 
-
-
-//CREATE array that the generator will choose from for alphabet, numeric and special characters
-var specialCharactersList = "!#$%&()*+,-./:;<=>?@[]^_{|}~".split("")
 var characters = {
   lowercase: function () {
     var character = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 1)
@@ -52,40 +41,38 @@ var characters = {
     // console.log(Math.floor(Math.random() * 9));
   },
   specialCharacters: function () {
+    var specialCharactersList = "!#$%&()*+,-./:;<=>?@[]^_{|}~".split("")
     var character = specialCharactersList[Math.floor(Math.random() * 28)]
     return character
     // console.log(specialCharactersList[Math.floor(Math.random() * 28)]);
   }
 };
-// var characterType = ["characters.lowercase","characters.uppercase","characters.numeric","characters.specialCharacters"]
 
 // characters.lowercase()
 // characters.uppercase()
 // characters.numeric()
 // characters.specialCharacters()
 
-// var charBank = "";
-// console.log(charBank)
-
-
 function generatePassword() {
   //on click, begin with confirms on whether they would like certain criteria
 
-  //PASSWORD LENGTH/////////////////////////////////////////////////////////////////////////
+  //PASSWORD LENGTH
   criteria.length = parseInt(prompt("Please specify a password length (Min: 8, Max: 128). If this not specified, the default length will be 8 characters.","8"))
   //if they do not enter any values, default the password length to 8.
   if (criteria.length === "") {
     criteria.length = 8;
   }
+  //check is the entered length is a non numeric value 
   if (isNaN(parseInt(criteria.length)) === true) {
     criteria.length = prompt("Please specify a numeric value between 8 and 128 for the password length.")
   }
+  //check that it has the correct length range
   if (criteria.length < 8 || criteria.length > 128) {
     criteria.length = prompt("The length specified is outside the range allowed. Please choose a password length between 8 and 128 characters:")
   }
-  console.log(criteria.length)
+  // console.log(criteria.length)
 
-  //CHARACTERS/////////////////////////////////////////////////////////////////////////
+  //CHARACTERS//
   alert("The following prompts will be used to determine the different character types included in your new password. At least one character type must be selected.")
 
   //if every character type in the criteria object is still set to false then alert that they need to select one and loop back through. 
@@ -107,10 +94,9 @@ function generatePassword() {
     }
   }
 
+
+
   var charBank = "";
-
-
-  // var stringlength = 0;
 
   // if the counter is less than the length of the password. 
   for (var i = 0; i < criteria.length;) {
@@ -144,16 +130,16 @@ function generatePassword() {
   console.log(charBank)
 
   splitPass = charBank.split("")
-  
+  //shuffle the character bank so that it is not always in the order of the for loop above. 
   for (var i = splitPass.length -  1; i > 0; i--) {
     var random = Math.floor(Math.random() * (i + 1))
     var switchChar = splitPass[i]
     splitPass[i] = splitPass[random]
     splitPass[random] = switchChar;
   }
-  console.log(splitPass)
-  // password = passwordShuffle(charBank);
-  // console.log(password)
+  // console.log(splitPass)
+
+  //join the array back together as a string
   password = splitPass.join("");
   return password
 }
